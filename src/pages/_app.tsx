44fc222c27f1ac5517bc9f9355a5a5e5d7e16509
Amanda Cleto <amanda.clet@gmail.com>
2021-05-03 21:04:50 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../sytles/global.scss';
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>oi</h1>
+      <h2>oi</h2>
+    </div>
+  )
+}
